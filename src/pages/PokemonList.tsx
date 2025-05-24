0541b905b5a8528602eb789b0b8f54ea67784cf4
fvester <wt0630@naver.com>
2025-05-24 14:03:02 +0900

    🎨 Add total routing

---
+++ src/pages/PokemonList.tsx
@@ -0,0 +1,11 @@
+import './PokemonList.scss';
+
+const PokemonList = () => {
+  return (
+    <div id="pokemon-list">
+      <h1>Pokemon List</h1>
+    </div>
+  );
+};
+
+export default PokemonList;
