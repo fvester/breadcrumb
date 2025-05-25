5353fb6c56c4e20eabe2749b388266ba045f858c
fvester <wt0630@naver.com>
2025-05-26 08:44:48 +0900

    ✨ Logo click to go home

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import './Header.scss';
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="header">
       <div className="header-container">
-        <div className="header-title">
+        <div
+          className="header-title"
+          onClick={(e: React.MouseEvent) => navigate('/')}
+          style={{ cursor: 'pointer' }}
+        >
           <span>Pokédex</span>
         </div>
         <div className="header-link"></div>
