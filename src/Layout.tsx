7c6c9314846ff89991269a8d07ff137a74e270da
fvester <wt0630@naver.com>
2025-05-26 13:13:29 +0900

    💄 Add Auto scroll to top

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,15 +2,19 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/common/Header';
 import './Layout.scss';
 import Footer from './components/common/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 // For entire layout
 const Layout = () => {
   return (
-    <div className="layout">
-      <Header />
-      <Outlet /> {/* Render child routes */}
-      <Footer />
-    </div>
+    <>
+      <ScrollToTop />
+      <div className="layout">
+        <Header />
+        <Outlet /> {/* Render child routes */}
+        <Footer />
+      </div>
+    </>
   );
 };
 
