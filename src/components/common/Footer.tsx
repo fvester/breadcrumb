7f202aea8695c523464116aa0f58fd43b818667c
fvester <wt0630@naver.com>
2025-05-26 13:41:44 +0900

    💄 Add notosans font

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -8,13 +8,25 @@ const Footer: React.FC = () => {
           <div className="footer-left-content">
             <div className="footer-left-title">Info</div>
             <ul className="footer-left-fields">
-              <li>tel: </li>
-              <li>email: </li>
+              <li>☎ tel: 010-5766-1656</li>
+              <li>✉️ email: kdm980630@gmail.com</li>
+              <li>
+                🍀 github:{' '}
+                <a
+                  href="https://github.com/fvester"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  https://github.com/fvester
+                </a>
+              </li>
             </ul>
           </div>
         </div>
         <div className="footer-right">
-          <div className="footer-right-content">right</div>
+          <div className="footer-right-content">
+            © 2024. Fvester. All rights reserved
+          </div>
         </div>
       </div>
     </div>
