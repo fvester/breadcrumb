40b419178250b04c813225a2b695a38eab67e73a
fvester <wt0630@naver.com>
2025-05-26 11:22:31 +0900

    💄 Add Pokemon list page

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -4,8 +4,18 @@ const Footer: React.FC = () => {
   return (
     <div className="footer">
       <div className="footer-container">
-        <div className="footer-left">left</div>
-        <div className="footer-right">right</div>
+        <div className="footer-left">
+          <div className="footer-left-content">
+            <div className="footer-left-title">Info</div>
+            <ul className="footer-left-fields">
+              <li>tel: </li>
+              <li>email: </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-right">
+          <div className="footer-right-content">right</div>
+        </div>
       </div>
     </div>
   );
