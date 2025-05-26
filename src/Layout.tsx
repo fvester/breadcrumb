a76fff68ed2fec68b4de2056ab98cb91f64792ff
fvester <wt0630@naver.com>
2025-05-26 14:48:17 +0900

    💄 Add route toast message

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,11 +3,14 @@ import Header from './components/common/Header';
 import './Layout.scss';
 import Footer from './components/common/Footer';
 import ScrollToTop from './components/ScrollToTop';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 // For entire layout
 const Layout = () => {
   return (
     <>
+      <ToastContainer />
       <ScrollToTop />
       <div className="layout">
         <Header />
